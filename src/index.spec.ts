dbf008303eefa9ce89265308fc8800b17bc7392f
Rueihuan <rueihuan@outlook.com>
2021-02-05 12:51:41 -0500

    test: tweak

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,37 +1,41 @@
-import { singleton, Singleton, SINGLETON_KEY } from './index'
+import { singleton, Singleton, SINGLETON_KEY } from "./index";
 
-describe('Singleton', () => {
-  it('Makes class a singleton', () => {
+describe("Singleton", () => {
+  it("makes class a singleton", () => {
     @singleton
     class Test {}
 
-    const instance1 = new Test()
-    const instance2 = new Test()
-    expect(instance1).toBe(instance2)
-    const instanceSingleton = (Test as Singleton<typeof Test>)[SINGLETON_KEY]
-    expect(instanceSingleton).toBe(instance1)
-  })
+    const instance1 = new Test();
+    const instance2 = new Test();
+    expect(instance1).toBe(instance2);
+    const instanceSingleton = (Test as Singleton<typeof Test>)[SINGLETON_KEY];
+    expect(instanceSingleton).toBe(instance1);
+  });
 
-  it('Makes class a singleton - non-decorator based', () => {
+  it("makes class a singleton - non-decorator based", () => {
     class Test {}
-    const TestSingleton = singleton(Test)
+    const TestSingleton = singleton(Test);
 
-    const instance1 = new TestSingleton()
-    const instance2 = new TestSingleton()
-    expect(instance1).toBe(instance2)
-    const instanceSingleton = (TestSingleton as Singleton<typeof TestSingleton>)[SINGLETON_KEY]
-    expect(instanceSingleton).toBe(instance1)
-  })
+    const instance1 = new TestSingleton();
+    const instance2 = new TestSingleton();
+    expect(instance1).toBe(instance2);
+    const instanceSingleton = (TestSingleton as Singleton<
+      typeof TestSingleton
+    >)[SINGLETON_KEY];
+    expect(instanceSingleton).toBe(instance1);
+  });
 
-  it("Doesn't make child class a singleton", () => {
+  it("does not make child class a singleton", () => {
     @singleton
     class Parent {}
     class Child extends Parent {}
 
-    const instance1 = new Child()
-    const instance2 = new Child()
-    expect(instance1).not.toBe(instance2)
-    const instanceSingleton = (Parent as Singleton<typeof Parent>)[SINGLETON_KEY]
-    expect(instanceSingleton).toBe(undefined)
-  })
-})
+    const instance1 = new Child();
+    const instance2 = new Child();
+    expect(instance1).not.toBe(instance2);
+    const instanceSingleton = (Parent as Singleton<typeof Parent>)[
+      SINGLETON_KEY
+    ];
+    expect(instanceSingleton).toBe(undefined);
+  });
+});
